70e7dbb8078aab317f9ac4bbdfd51bb27488399f
menghengleap <menghengleap@yahoo.com>
2024-03-21 15:25:42 -0500

    5th

--- Database.php
+++ Database.php
@@ -23,7 +23,7 @@ class Database {
     
         try {
             // DSN without the client_encoding option
-            $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name;
+            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
