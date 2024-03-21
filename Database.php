4c20b297bf00d04b9860fc257c9ebc50519724a4
menghengleap <menghengleap@yahoo.com>
2024-03-21 15:14:43 -0500

    3rd commit

--- Database.php
+++ Database.php
@@ -20,16 +20,17 @@ class Database {
     // Getting the database connection
     public function getConnection() {
         $this->conn = null;
-
+    
         try {
-            $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";charset=utf8";
+            // Corrected DSN string for PostgreSQL
+            $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name . ";options='--client_encoding=UTF8'";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
-
+    
         return $this->conn;
     }
 }
