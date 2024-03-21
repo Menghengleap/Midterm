bc38e7c1174dd5e439d964fa4079ce31c3fc47ee
menghengleap <menghengleap@yahoo.com>
2024-03-21 15:46:17 -0500

    8th

--- Database.php
+++ Database.php
@@ -30,11 +30,14 @@ class Database {
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $this->conn;
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
+            
         }
     
-        return $this->conn;
+        
+        
     }
     
 }
